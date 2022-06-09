851cfe3fb535bfed9dda2186c41ef56f36ece10d
Rafal <magiedeszu@gmail.com>
2022-06-09 22:20:22 +0200

    first commit

---
+++ html_editor.py
@@ -0,0 +1,32 @@
+from html.parser import HTMLParser
+import codecs
+from bs4 import BeautifulSoup
+import os
+import csv
+rel_csv_path = "contacts.csv"
+script_dir = os.path.dirname(__file__)
+rel_path = "index_edited_2.html"
+abs_file_path = os.path.join(script_dir, rel_path)
+abs_csv_file_path = os.path.join(script_dir, rel_csv_path)
+f=codecs.open(abs_file_path, 'r',"utf-8")
+
+document= BeautifulSoup(f.read(), "html.parser")
+mydivs_name = document.find(class_="name")
+mydivs_number = document.find(class_="number")
+mydivs_mail = document.find(class_="mail")
+mydivs_position = document.find(class_="position")
+
+with open(abs_csv_file_path, newline='', encoding="utf8") as csvfile:
+    spamreader = csv.reader(csvfile)
+    for row in spamreader:
+        mydivs_name.string = row[0] + " " + row[1]
+        mydivs_mail.find('a')['href'] ="mailto:"+row[3]
+        mydivs_number.find('a')['href'] ="tel:+48"+row[2]
+        name = row[0] + " " + row[1]
+        mydivs_number.find('a').string.replace_with(row[2])
+        mydivs_mail.find('a').string.replace_with(row[3])
+        mydivs_position.string = row[4]
+        html = document.prettify("utf-8")
+        filename = row[0] + "_" + row[1] + ".html"
+        with open(filename, "wb") as file:
+            file.write(html)
